1d1280ce635ac1420d0d393ebea24fb7268fce79
Jack East <23142577+jackeast23@users.noreply.github.com>
2021-03-05 18:55:44 -0500

    added background and time

--- main.py
+++ main.py
@@ -2,15 +2,27 @@ import pygame
 
 WIDTH, HEIGHT = 900, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("First Game!")
+
+WHITE = (255, 255, 255)
+
+FPS = 60
+
+def draw_window():
+    WIN.fill((WHITE))
+    pygame.display.update()
+
 
 def main():
-
+    clock = pygame.time.clock()
     run = True
     while run:
-
+        clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+
+        draw_window()
 
     pygame.quit()
 
