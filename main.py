161b89fae8fd064388214f17c30f42ffb61d0620
Jack East <23142577+jackeast23@users.noreply.github.com>
2021-03-08 20:03:39 -0500

    added border for each player

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("First Game!")
 
 WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+BORDER = pygame.Rect((WIDTH/2)-5, 0, 10, HEIGHT)
 
 FPS = 60
 VEL = 5
@@ -23,28 +26,29 @@ RED_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(
 
 def draw_window(red, yellow):
     WIN.fill((WHITE))
+    pygame.draw.rect(WIN, BLACK, BORDER)
     WIN.blit(YELLOW_SPACESHIP, (yellow.x, yellow.y))
     WIN.blit(RED_SPACESHIP, (red.x, red.y))
     pygame.display.update()
 
 def yellow_handle_movement(keys_pressed, yellow):
-    if keys_pressed[pygame.K_a]: # LEFT
+    if keys_pressed[pygame.K_a] and yellow.x - VEL > 0: # LEFT
         yellow.x -= VEL
-    if keys_pressed[pygame.K_d]: # RIGHT
+    if keys_pressed[pygame.K_d] and yellow.x + VEL + yellow.width < BORDER.x + 11: # RIGHT
         yellow.x += VEL
-    if keys_pressed[pygame.K_w]: # UP
+    if keys_pressed[pygame.K_w] and yellow.y - VEL > 0: # UP
         yellow.y -= VEL
-    if keys_pressed[pygame.K_s]: # DOWN
+    if keys_pressed[pygame.K_s] and yellow.y + VEL + yellow.height < HEIGHT  - 15: # DOWN
         yellow.y += VEL
 
 def red_handle_movement(keys_pressed, red):
-    if keys_pressed[pygame.K_LEFT]: # LEFT
+    if keys_pressed[pygame.K_LEFT] and red.x - VEL > BORDER.x + BORDER.width: # LEFT
         red.x -= VEL
-    if keys_pressed[pygame.K_RIGHT]: # RIGHT
+    if keys_pressed[pygame.K_RIGHT] and red.x + VEL + red.width < WIDTH + 15: # RIGHT
         red.x += VEL
-    if keys_pressed[pygame.K_UP]: # UP
+    if keys_pressed[pygame.K_UP] and red.y - VEL > 0: # UP
         red.y -= VEL
-    if keys_pressed[pygame.K_DOWN]: # DOWN
+    if keys_pressed[pygame.K_DOWN] and red.y + VEL + red.height < HEIGHT  - 15: # DOWN
         red.y += VEL
 
 def main():
